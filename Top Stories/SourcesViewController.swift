81fe4065b61d2ca3564af07b03edc5865fd9121c
Jean Cho <jeancho@jeans-macbook-air.local>
2018-07-10 10:41:54 -0500

    Finished initial set up of SourcesTableView", and push your changes up to GitHub.

--- Top Stories/SourcesViewController.swift
+++ Top Stories/SourcesViewController.swift
@@ -48,7 +48,18 @@ class SourcesViewController: UITableViewController {
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil) }
     
-
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sources.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let source = sources[indexPath.row]
+        cell.textLabel?.text = source["name"]
+        cell.detailTextLabel?.text = source["description"]
+        return cell
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -56,4 +67,28 @@ class SourcesViewController: UITableViewController {
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
